c8e30bceb7b421d9068bf28c17d2b551790cbbf6
Ryuga <ribereo.minami@gmail.com>
2023-03-09 23:31:02 +0900

    Initial Commit

---
+++ Kadai9-SwiftUI/Kadai9_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Kadai9_SwiftUIApp.swift
+//  Kadai9-SwiftUI
+//
+//  Created by Ryuga on 2023/03/09.
+//
+
+import SwiftUI
+
+@main
+struct Kadai9_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
