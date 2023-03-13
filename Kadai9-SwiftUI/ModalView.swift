3531da3619216f2167c7535063b090b387d593c6
Ryuga <ribereo.minami@gmail.com>
2023-03-13 17:34:47 +0900

    modalの実装

---
+++ Kadai9-SwiftUI/ModalView.swift
@@ -0,0 +1,49 @@
+//
+//  ModalView.swift
+//  Kadai9-SwiftUI
+//
+//  Created by Ryuga on 2023/03/12.
+//
+
+import SwiftUI
+
+struct ModalView: View {
+    @Environment(\.presentationMode) var presentationMode
+    @Binding var selection: String
+    let options = ["東京都", "神奈川県", "埼玉県", "千葉県"]
+
+    var body: some View {
+        NavigationView {
+            VStack {
+                ForEach(options, id: \.self) { option in
+                    Button(action: {
+                        selection = option
+                        presentationMode.wrappedValue.dismiss()
+                    }, label: {
+                        Text(option)
+                            .padding(10)
+                    })
+                }
+                .navigationBarTitle("ModalView")
+                .navigationBarTitleDisplayMode(.inline)
+                .navigationBarItems(leading: Button("Cancel") {
+                    self.presentationMode.wrappedValue.dismiss()
+                })
+                Spacer()
+            }
+        }
+    }
+}
+
+struct Preview: View {
+    @State var selection = "未選択"
+    var body: some View {
+        ModalView(selection: $selection)
+    }
+}
+
+struct ModalView_Previews: PreviewProvider {
+    static var previews: some View {
+        Preview()
+    }
+}
